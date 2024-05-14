08755cb7e57b4274c74cce115713523168bb364b
Greegs26 <greganchristian@gmail.com>
2024-05-14 20:46:52 -0300

    mergin all to master branch, trying to see activity on profile

--- EulerSampleCode.py
+++ EulerSampleCode.py
@@ -1,19 +1,21 @@
 # reprogramming Matlab codes to Python from Electrical Engineering degree for fun
+#try:
+#    import numpy as np
+#    print("NumPy is installed. Version:", np.__version__)
+#except ImportError:
+#    print("NumPy is not installed.")
+import numpy as np
+
 h=0.01
 a=5
 b=15
 t0=a
-#y0=exp(t0)-t0-1
+y0=np.exp(t0)-t0-1
 
 
 n=(b-a)/h
-#t=zeros(n,1)
+t=np.zeros(n,1)
 
-try:
-    import numpy as np
-    print("NumPy is installed. Version:", np.__version__)
-except ImportError:
-    print("NumPy is not installed.")
 
 
 
